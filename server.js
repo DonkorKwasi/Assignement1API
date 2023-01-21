4dcd51659d7b502e766dd9ce1d6e237a6ed0a8be
kwasi <kwasi.adom.donkor@gmail.com>
2023-01-21 02:44:44 +0000

    port

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv').config(); 
 const app = express();
-const HTTP_PORT = 8080
 app.use(cors());
 app.use(express.json());
 const MoviesDB = require("./modules/moviesDB.js");
